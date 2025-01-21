a949ef4e653c8aa557fe32be5d03067dc3252a1d
Lorenzo Bertazzo <127543028+UberPiccion@users.noreply.github.com>
2025-01-21 11:42:27 +0100

    Setup Project

---
+++ Source/PlasticBeachJam2025/PlasticBeachJam2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlasticBeachJam2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlasticBeachJam2025, "PlasticBeachJam2025" );
